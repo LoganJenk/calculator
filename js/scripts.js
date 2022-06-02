1ac1ccb9b559e59e449014f0fcae73a2ad0b66bf
Logan Jenkins <LoganJenk1@gmail.com>
2022-06-02 11:26:06 -0700

    add addition, subtraction, multiplication, & division buttons

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,26 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    alert(result);
+    $("#output1").text(result);
   });
+
+  $("form#subtract").submit(function(event) { 
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#output2").text(result);
+
+  });
+
+
+
+
+
+
+
+
 });
+
+
+
